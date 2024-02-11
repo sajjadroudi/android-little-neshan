05e368423c53202fbcc896f0cfd732bf64a1ca2e
roudi <sajjad.roodi@gmail.com>
2024-02-11 18:24:38 +0330

    Add getting address endpoint to neshan service

--- app/src/main/java/ir/roudi/littleneshan/NeshanService.java
+++ app/src/main/java/ir/roudi/littleneshan/NeshanService.java
@@ -1,4 +1,12 @@
 package ir.roudi.littleneshan;
 
+import io.reactivex.rxjava3.core.Single;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface NeshanService {
+
+    @GET("reverse")
+    Single<AddressResponse> getAddress(@Query("lat") long lat, @Query("lng") long lng);
+
 }
