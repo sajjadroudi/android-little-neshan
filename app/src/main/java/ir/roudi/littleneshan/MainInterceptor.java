d6f98229beee057ca954902c580d581a11a97668
roudi <sajjad.roodi@gmail.com>
2024-02-11 18:33:43 +0330

    Initialize Retrofit

---
+++ app/src/main/java/ir/roudi/littleneshan/MainInterceptor.java
@@ -0,0 +1,34 @@
+package ir.roudi.littleneshan;
+
+import androidx.annotation.NonNull;
+
+import java.io.IOException;
+
+import okhttp3.Headers;
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainInterceptor implements Interceptor {
+
+    private static final String KEY_API_KEY = "Api-Key";
+    private static final String VALUE_API_KEY = "service.9c22024b43d14d749c794c2a280a90d6";
+
+    @NonNull
+    @Override
+    public Response intercept(@NonNull Chain chain) throws IOException {
+        Request request = chain.request();
+
+        Headers headers = request.headers()
+                .newBuilder()
+                .add(KEY_API_KEY, VALUE_API_KEY)
+                .build();
+
+        Request newRequest = request.newBuilder()
+                .headers(headers)
+                .build();
+
+        return chain.proceed(newRequest);
+    }
+
+}
---
+++ app/src/main/java/ir/roudi/littleneshan/RetrofitHelper.java
@@ -0,0 +1,30 @@
+package ir.roudi.littleneshan;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitHelper {
+
+    private static final OkHttpClient client = new OkHttpClient.Builder()
+            .connectTimeout(5, TimeUnit.SECONDS)
+            .readTimeout(5, TimeUnit.SECONDS)
+            .callTimeout(5, TimeUnit.SECONDS)
+            .addInterceptor(new MainInterceptor())
+            .build();
+
+    private static final Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("https://api.neshan.org/v4/")
+            .client(client)
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+
+    public static NeshanService createNeshanService() {
+        return retrofit.create(NeshanService.class);
+    }
+
+}
