96a6d1cfd2ef9cdf01ee1b50519d018061404650
roudi <sajjad.roodi@gmail.com>
2024-02-15 07:31:53 +0330

    Add custom application class

---
+++ app/src/main/java/ir/roudi/littleneshan/LittleNeshanApplication.java
@@ -0,0 +1,10 @@
+package ir.roudi.littleneshan;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class LittleNeshanApplication extends Application {
+
+}
