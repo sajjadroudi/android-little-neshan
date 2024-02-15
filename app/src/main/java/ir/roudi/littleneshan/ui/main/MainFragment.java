b1487ad8db376cc07503b677fbdd58a0f541571b
roudi <sajjad.roodi@gmail.com>
2024-02-15 14:37:04 +0330

    Mark user location on map

--- app/src/main/java/ir/roudi/littleneshan/ui/main/MainFragment.java
+++ app/src/main/java/ir/roudi/littleneshan/ui/main/MainFragment.java
@@ -1,5 +1,6 @@
 package ir.roudi.littleneshan.ui.main;
 
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,12 +9,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.carto.styles.MarkerStyle;
+import com.carto.styles.MarkerStyleBuilder;
+import com.carto.utils.BitmapUtils;
+
+import org.neshan.mapsdk.model.Marker;
+
+import ir.roudi.littleneshan.R;
 import ir.roudi.littleneshan.data.model.LocationModel;
 import ir.roudi.littleneshan.databinding.FragmentMainBinding;
 
 public class MainFragment extends Fragment {
 
     private FragmentMainBinding binding;
+    private Marker userMarker;
 
     @Override
     public View onCreateView(
@@ -39,6 +48,24 @@ public class MainFragment extends Fragment {
     private void focusOnLocation(LocationModel location) {
         binding.map.moveCamera(location.toLatLng(), 0.25f);
         binding.map.setZoom(15, 0.25f);
+    }
+
+    private void markUserOnMap(LocationModel location, boolean isCachedLocation) {
+        if(userMarker != null) {
+            binding.map.removeMarker(userMarker);
+        }
+        userMarker = new Marker(location.toLatLng(), buildUserMarkerStyle(isCachedLocation));
+        binding.map.addMarker(userMarker);
+    }
+
+    private MarkerStyle buildUserMarkerStyle(boolean isCachedLocation) {
+        MarkerStyleBuilder markStCr = new MarkerStyleBuilder();
+        markStCr.setSize(30f);
+        var drawable = isCachedLocation ? R.drawable.ic_marker_off : R.drawable.ic_marker;
+        var androidBitmap = BitmapFactory.decodeResource(getResources(), drawable);
+        var bitmap = BitmapUtils.createBitmapFromAndroidBitmap(androidBitmap);
+        markStCr.setBitmap(bitmap);
+        return markStCr.buildStyle();
     }
 
 }
