1939526f779ee74bb92b71995267762512d2381c
roudi <sajjad.roodi@gmail.com>
2024-02-15 07:19:16 +0330

    Request location permission

--- app/src/main/java/ir/roudi/littleneshan/MainActivity.java
+++ app/src/main/java/ir/roudi/littleneshan/MainActivity.java
@@ -1,8 +1,14 @@
 package ir.roudi.littleneshan;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        requestPermissionLocationIfNotGranted();
     }
+
+    private void requestPermissionLocationIfNotGranted() {
+        if(!isPermissionGranted(android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+            ActivityResultLauncher<String> result = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+                @Override
+                public void onActivityResult(Boolean isGranted) {
+                    // TODO: Handle later
+                    Toast.makeText(
+                            MainActivity.this,
+                            "Location Permission Status: " + isGranted,
+                            Toast.LENGTH_LONG
+                    ).show();
+                }
+            });
+            result.launch(android.Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+    }
+
+    private boolean isPermissionGranted(String permission) {
+        return ContextCompat.checkSelfPermission(getApplicationContext(), permission)
+                == PackageManager.PERMISSION_GRANTED;
+    }
+
 }
