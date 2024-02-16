74d5c568214cdc601c275ca651d3079ddec814b6
roudi <sajjad.roodi@gmail.com>
2024-02-16 20:04:13 +0330

    Add logging interceptor

--- app/src/main/java/ir/roudi/littleneshan/di/NetworkModule.java
+++ app/src/main/java/ir/roudi/littleneshan/di/NetworkModule.java
@@ -9,6 +9,7 @@ import dagger.hilt.components.SingletonComponent;
 import ir.roudi.littleneshan.data.remote.MainInterceptor;
 import ir.roudi.littleneshan.data.remote.NeshanService;
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -34,14 +35,23 @@ public class NetworkModule {
 
     @Provides
     public static OkHttpClient provideOkHttpClient(
-            MainInterceptor interceptor
+            MainInterceptor interceptor,
+            HttpLoggingInterceptor loggingInterceptor
     ) {
         return new OkHttpClient.Builder()
                 .connectTimeout(5, TimeUnit.SECONDS)
                 .readTimeout(5, TimeUnit.SECONDS)
                 .callTimeout(5, TimeUnit.SECONDS)
                 .addInterceptor(interceptor)
+                .addInterceptor(loggingInterceptor)
                 .build();
+    }
+
+    @Provides
+    public static HttpLoggingInterceptor provideLoggingInterceptor() {
+        var interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        return interceptor;
     }
 
 }
