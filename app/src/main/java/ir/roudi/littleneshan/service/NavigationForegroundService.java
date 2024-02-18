ef30203ff2c9f28312e9b1266d1a44fc872718d9
roudi <sajjad.roodi@gmail.com>
2024-02-18 06:12:10 +0330

    Create notification channel

--- app/src/main/java/ir/roudi/littleneshan/service/NavigationForegroundService.java
+++ app/src/main/java/ir/roudi/littleneshan/service/NavigationForegroundService.java
@@ -1,12 +1,15 @@
 package ir.roudi.littleneshan.service;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 
 import javax.inject.Inject;
 
@@ -22,6 +25,15 @@ public class NavigationForegroundService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private NotificationChannel buildNotificationChannel() {
+        return new NotificationChannel(
+                "little-neshan-navigation-notification-channel",
+                "Navigation",
+                NotificationManager.IMPORTANCE_DEFAULT
+        );
     }
 
 }
