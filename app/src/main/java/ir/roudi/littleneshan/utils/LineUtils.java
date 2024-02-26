aaf7e05e4aa930dd28fcfb79293db50644cddf54
roudi <sajjad.roodi@gmail.com>
2024-02-27 00:38:36 +0330

    Clean up LineUtils

--- app/src/main/java/ir/roudi/littleneshan/utils/LineUtils.java
+++ app/src/main/java/ir/roudi/littleneshan/utils/LineUtils.java
@@ -12,6 +12,10 @@ import ir.roudi.littleneshan.R;
 
 public class LineUtils {
 
+    private LineUtils() {
+
+    }
+
     public static LineStyle buildLineStyle(Context context) {
         var builder = new LineStyleBuilder();
         var color = new Color(ContextCompat.getColor(context, R.color.blue));
