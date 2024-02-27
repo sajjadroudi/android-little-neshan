03004fe44c73de2b9ea11e5ebd6a9e1ab8070b1b
roudi <sajjad.roodi@gmail.com>
2024-02-27 08:43:18 +0330

    Finalize project

--- app/src/main/java/ir/roudi/littleneshan/core/Config.java
+++ app/src/main/java/ir/roudi/littleneshan/core/Config.java
@@ -6,6 +6,6 @@ public class Config {
 
     public static final boolean SHOW_USER_LOCATION_BY_NESHAN = true;
 
-    public static final boolean USE_FAKE_USER_LOCATION = true;
+    public static final boolean USE_FAKE_USER_LOCATION = false;
 
 }
